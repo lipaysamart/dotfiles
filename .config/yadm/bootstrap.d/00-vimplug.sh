9ddb9cc705eefcb4564c86ef401f8f2e2b6c65d6
lipaysam <lipaysamart@gmail.com>
2026-03-25 21:48:40 +0800

    fix

--- .config/yadm/bootstrap.d/00-vimplug.sh
+++ .config/yadm/bootstrap.d/00-vimplug.sh
@@ -1,4 +1,18 @@
-if command -v vim >/dev/null 2>&1; then
-  echo "Bootstraping Vim"
-  vim '+PlugUpdate' '+PlugClean!' '+PlugUpdate' '+qall'
+#!/bin/bash
+set -euo pipefail
+
+if ! command -v vim >/dev/null 2>&1; then
+    exit 0
 fi
+
+VIM_AUTOLOAD_DIR="${XDG_DATA_HOME:-$HOME/.vim/autoload"
+VIM_PLUG_FILE="$VIM_AUTOLOAD_DIR/plug.vim"
+
+if [[ ! -f "$VIM_PLUG_FILE" ]]; then
+    echo "正在安装 vim-plug..."
+    curl -fsSLo "$VIM_PLUG_FILE" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "正在 Bootstrap Vim..."
+vim '+PlugUpdate' '+PlugClean!' '+PlugUpdate' '+qall'
